9b93a76ded3c146e11802df9e0db1fb89629668a
Tim Horton <tmhorton@gmail.com>
2014-02-02 20:55:11 -0800

    Changed bounding box data representation

--- airbitz/restapi/serializers.py
+++ airbitz/restapi/serializers.py
@@ -37,7 +37,7 @@ class BoundingBoxField(fields.Field):
     type_name = 'BoundingBoxField'
 
     def field_to_native(self, obj, field_name):
-        return "[0.0, 0.0, 1.0, 0.25]"
+        return "{x: 0.0, y: 0.0, height: 0.25, width: 1.0}"
 
 class BusinessHoursSerializer(serializers.ModelSerializer):
     class Meta:
