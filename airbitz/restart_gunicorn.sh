42a53692f79b44daaed92dc01a2fdf389db5dec0
Tim Horton <tmhorton@gmail.com>
2014-03-18 21:52:26 -0700

    Really fixed gunicorn

--- airbitz/restart_gunicorn.sh
+++ airbitz/restart_gunicorn.sh
@@ -3,7 +3,7 @@ echo "Stopping gunicorn_django processes"
 killall gunicorn_django
 echo "Processes stopped."
 
-source $HOME/breadsticks/ENV/bin/activate
+source $HOME/airbitz/ENV/bin/activate
 if [[ -e /etc/profile.d/environment_vars.sh ]]; then
     source /etc/profile.d/environment_vars.sh
 fi
@@ -12,5 +12,5 @@ LOGFILE=/tmp/gunicorn.log
 NUM_WORKERS=4
 echo ""
 echo "Starting gunicorn"
-gunicorn_django --daemon -w $NUM_WORKERS --log-file=$LOGFILE 2>>$LOGFILE
+gunicorn airbitz.wsgi:application  -w $NUM_WORKERS --log-file=$LOGFILE 2>>$LOGFILE
 echo "Gunicorn started. Logs can be found at $LOGFILE"
