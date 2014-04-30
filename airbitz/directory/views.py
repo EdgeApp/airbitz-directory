2e461df0195676f804a1758de76a78bd0c75aceb
Damian Cutillo <damian@designseohosting.com>
2014-04-29 18:43:42 -0700

    Added Quebec and Netherlands to vector map

--- airbitz/directory/views.py
+++ airbitz/directory/views.py
@@ -93,8 +93,13 @@ ACTIVE_REGIONS = {
               'search': 'Ontario, Canada'},
     'CA-BC': {'name': 'British Columbia',
               'search': 'Ontario, Canada'},
-    # 'CA-QC': {'name': 'Quebec',
-    #           'search': 'Quebec, Canada'},
+    'CA-QC': {'name': 'Quebec',
+              'search': 'Quebec, Canada'},
+
+    'NL': {'name': 'Netherlands',
+           'search': 'Netherlands'},
+
+
 }
 
 
