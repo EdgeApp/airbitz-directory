e0757b9fb916613a77df4a5dfafbffbeef41a030
Damian <i3inary@gmail.com>
2014-06-02 14:13:39 -0700

    - Switched from using subprocess from python2.7 to using a backport (https://pypi.python.org/pypi/subprocess32/) which has timeout feature available - Added timeout to screencap() - Added error handling for non-zero statuses - Changed query from published to modified since newly modified businesses will need a new screenshot potentially - Script now accepts cmd line arguments for business ids

--- airbitz/cron-screencap.py
+++ airbitz/cron-screencap.py
@@ -1,6 +1,9 @@
-import subprocess
 import os
+import sys
+import subprocess32 as subprocess
 import datetime
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'airbitz.settings')
 from airbitz import settings
 from directory.models import Business
 
@@ -8,44 +11,56 @@ from directory.models import Business
 Makes screencapture image based on Airbitz Buisness ID
 '''
 def screencap(biz_id):
+    casper_timeout = 30
     casper_script = os.getcwd() + '/biz-screen-capture.js'
     casper_save = '--save=' + settings.MEDIA_ROOT + '/screencaps/'
     casper_url = '--url=' + settings.SCREENCAP_ABSOLUTE_URL
     casper_args = ' '.join(['casperjs', casper_script, casper_save, casper_url, str(biz_id)])
-    print casper_args
-    print subprocess.check_output(casper_args, shell=True)
+    print 'CASPER CMD:', casper_args
+
+    try:
+        print subprocess.check_output(casper_args, shell=True, timeout=casper_timeout)
+    except subprocess.TimeoutExpired:
+        print '**** CASPERJS TIME IS UP GOTTA MOVE ON ****'
+    except subprocess.CalledProcessError as e:
+        print '*** CASPER CMD ERROR CODE = ', e.returncode
+
 
 '''
-Queries for businesses published in the window given
+Queries for businesses modified in the window given
 '''
 def get_biz_list(d1=datetime.date.today(), d2=None):
     b_list = []
     if not d2:
-        pub = Business.objects.filter(status="PUB").filter(published__gte=d1)
+        pub = Business.objects.filter(status="PUB").filter(modified__gte=d1)
     else:
-        pub = Business.objects.filter(status="PUB").filter(published__gte=d1, published__lte=d2)
+        pub = Business.objects.filter(status="PUB").filter(modified__gte=d1, modified__lte=d2)
     for b in pub:
         b_list.append(b.id)
     b_list.sort(reverse=True)
-    print b_list, '\n', 'BUSINESSES FOUND:', pub.count(), '\n'
+    print b_list, '\n', 'BUSINESSES MODIFIED :', pub.count(), '\n'
     return b_list
 
 '''
-Makes screencapture images and consumes list of Airbitz business IDs that are published and live
+Makes screencapture images and consumes list of Airbitz business IDs that are modified and live
 '''
 def get_screencaps(b_list=get_biz_list()):
-    for id in b_list[:]:
-        screencap(id)
-        b_list.remove(id)
+    for bId in b_list[:]:
+        screencap(bId)
+        b_list.remove(bId)
         print 'NEXT 5:', str(b_list[:5]), str(len(b_list)), 'LEFT'
 
 
+date1 = datetime.date(2014, 5, 30)
+date2 = datetime.date.today
 
-
-date1 = datetime.date(2014, 5, 29)
-date2 = date1 + datetime.timedelta(days=1)
-
-get_biz_list()
+biz_list = get_biz_list(date1)[35:]
 biz_list.sort(reverse=True)
 
-get_screencaps()
+'''
+Check if argument was passed from CMD line
+'''
+if len(sys.argv) < 2:
+    get_screencaps(biz_list)
+else:
+    get_screencaps(sys.argv[1:])
