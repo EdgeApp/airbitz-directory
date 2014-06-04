ae14b927a3149b1fbe53a42eebe03f7367d6d7fe
Damian <i3inary@gmail.com>
2014-06-04 14:13:43 -0700

    Updated push scripts so that files are never deleted

--- staging/push_screencaps_staging.sh
+++ staging/push_screencaps_staging.sh
@@ -8,9 +8,7 @@
 STAGING_SERVER_USER="devbitz"
 STAGING_SERVER_IP="198.61.228.9"
 
-RSYNC_SSH=
-
 RSYNC_SRC="/staging/media/screencaps/"
 RSYNC_DEST="${STAGING_SERVER_USER}@$STAGING_SERVER_IP:~/media/screencaps"
 
-rsync --progress --inplace --delete --exclude=".DS_Store" -avz --rsh 'ssh -i /staging/.ssh/dev_keys' $RSYNC_SRC $RSYNC_DEST
+rsync --progress --inplace --exclude=".DS_Store" -avz --rsh 'ssh -i /staging/.ssh/dev_keys' $RSYNC_SRC $RSYNC_DEST
