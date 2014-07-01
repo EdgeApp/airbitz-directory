b0315dfba3706aeb965484641e2b24ebe5dd5f3c
Damian <i3inary@gmail.com>
2014-07-01 15:37:15 -0700

    - Added update and remove function - Check if values are null before attempting

--- airbitz/airbitz/static/businessAdd/services/abDataFactory.js
+++ airbitz/airbitz/static/businessAdd/services/abDataFactory.js
@@ -9,11 +9,37 @@ var abDataFactory = function ($http, $q) {
 
 
 
-  factory.addData = function (data) {
-    console.log('addData:');console.log(data);
-    storage_array.push(data);
+  factory.appendData = function (data) {
+    console.log('appendData:');console.log(data);
+    if(data !== undefined) {
+      storage_array.push(data);
+    }
   };
 
+
+  factory.updateData = function (data) {
+    console.log('updateData UPDATING:');console.log(data);
+    if(data !== undefined) {
+      if(storage_array.length) {
+        factory.removeData(0);
+      }
+      storage_array.push(data);
+    }
+  };
+
+
+  factory.removeData = function(id) {
+    var index;
+    // checking for item in array
+    for(var i=0; i<storage_array.length; i++) {
+      if(id === storage_array[i].id){
+        console.log('removeData FOUND ' + storage_array[id]);
+        index = i;
+      }
+    }
+    console.log('removeData REMOVING: '); console.log(storage_array[id]);
+    storage_array.splice(index, 1);
+  };
 
 
   factory.getData = function () {
