a746501b278cf747f54d96e56915e1759068e0b1
Tim Horton <tim@airbitz.co>
2014-12-29 13:50:30 -0800

    Replace spaces with dash

--- airbitz/directory/search_indexes.py
+++ airbitz/directory/search_indexes.py
@@ -61,14 +61,13 @@ class BusinessIndex(CelerySearchIndex, indexes.SearchIndex, indexes.Indexable):
         return [category.name for category in obj.categories.all()]
 
     def prepare_phone(self, obj):
-        if True:
-            return obj.phone
         if not obj.phone:
             return None
         try:
             c = reverseCountryMap(obj.country)
             num = phonenumbers.parse(obj.phone, c)
-            return phonenumbers.format_number(num, phonenumbers.PhoneNumberFormat.INTERNATIONAL)
+            f = phonenumbers.format_number(num, phonenumbers.PhoneNumberFormat.INTERNATIONAL)
+            return f.replace(" ", "-")
         except:
             log.warn('Invalid number {0}:{1}'.format(obj.phone, obj.country))
             return obj.phone
