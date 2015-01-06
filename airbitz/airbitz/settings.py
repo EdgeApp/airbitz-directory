e8e60408b84be97843b9293a4215f727e9b27397
Damian <i3inary@gmail.com>
2015-01-06 15:28:47 -0800

    Add prism for code syntax highlighting

--- airbitz/airbitz/settings.py
+++ airbitz/airbitz/settings.py
@@ -231,6 +231,7 @@ PIPELINE_CSS = {
         'source_filenames': (
             'css/animate.css',
             'extras/Magnific-Popup-master/dist/magnific-popup.css',
+            'extras/prism/prism.css',
             'css/page-content.css',
         ),
         'output_filename': 'css/page-conent.' + DEPLOY_DATE + '.css',
@@ -349,6 +350,7 @@ PIPELINE_JS = {
             'extras/carouFredSel-6.2.1/jquery.carouFredSel-6.2.1-packed.js',
             'extras/Magnific-Popup-master/dist/jquery.magnific-popup.js',
             'extras/FitVids.js-master/jquery.fitvids.js',
+            'extras/prism/prism.js',
             'js/page_content.js',
         ),
         'output_filename': 'js/page-content.' + DEPLOY_DATE + '.js',
