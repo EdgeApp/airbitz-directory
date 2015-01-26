795d2bbea09e48cc043be621d461b4b806f7084f
Damian <i3inary@gmail.com>
2015-01-26 13:46:50 -0800

    Fixes for mobile device load time and layout

--- airbitz/airbitz/static/js/home-ui.js
+++ airbitz/airbitz/static/js/home-ui.js
@@ -88,54 +88,55 @@ jQuery(function($) {
   }, 5000);
 
 
-
-/* config dom id (optional) + config particles params */
-particlesJS('landing-particles', {
-  particles: {
-    color: '#fff',
-    shape: 'circle', // "circle", "edge" or "triangle"
-    opacity:.7,
-    size: 6,
-    size_random: true,
-    nb: 150,
-    line_linked: {
-      enable_auto: true,
-      distance: 300,
+if(!bowser.android && !bowser.ios ) {
+  /* config dom id (optional) + config particles params */
+  particlesJS('landing-particles', {
+    particles: {
       color: '#fff',
-      opacity:.4,
-      width: 2,
-      condensed_mode: {
-        enable: false,
-        rotateX: 600,
-        rotateY: 600
-      }
-    },
-    anim: {
-      enable: true,
-      speed:.75
-    }
-  },
-  interactivity: {
-    enable: true,
-    mouse: {
-      distance: 300
-    },
-    detect_on: 'canvas', // "canvas" or "window"
-    mode: 'grab',
-    line_linked: {
-      opacity: .4
-    },
-    events: {
-      onclick: {
+      shape: 'circle', // "circle", "edge" or "triangle"
+      opacity:.7,
+      size: 6,
+      size_random: true,
+      nb: 150,
+      line_linked: {
+        enable_auto: true,
+        distance: 300,
+        color: '#fff',
+        opacity:.4,
+        width: 2,
+        condensed_mode: {
+          enable: false,
+          rotateX: 600,
+          rotateY: 600
+        }
+      },
+      anim: {
         enable: true,
-        mode: 'push', // "push" or "remove"
-        nb: 4
+        speed:.75
       }
-    }
-  },
-  /* Retina Display Support */
-  retina_detect: true
-});
+    },
+    interactivity: {
+      enable: true,
+      mouse: {
+        distance: 300
+      },
+      detect_on: 'canvas', // "canvas" or "window"
+      mode: 'grab',
+      line_linked: {
+        opacity: .4
+      },
+      events: {
+        onclick: {
+          enable: true,
+          mode: 'push', // "push" or "remove"
+          nb: 4
+        }
+      }
+    },
+    /* Retina Display Support */
+    retina_detect: true
+  });
+}
 
 
 // BUTTON SCROLL AND DRAW ATTENTION
