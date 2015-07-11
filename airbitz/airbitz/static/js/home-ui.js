6384cd81195af9df1f4d0f10163eb6e351b359d7
Damian <i3inary@gmail.com>
2015-07-10 17:04:04 -0700

    Fix slider overlap home page

--- airbitz/airbitz/static/js/home-ui.js
+++ airbitz/airbitz/static/js/home-ui.js
@@ -38,7 +38,7 @@ jQuery(function($) {
     width: 406,
     height: 560,
     auto: {
-      play: true
+      play: false
     },
     scroll: {
       pauseOnHover: true,
@@ -98,55 +98,55 @@ jQuery(function($) {
   });
 
 
-//if(!bowser.android && !bowser.ios ) {
-//  /* config dom id (optional) + config particles params */
-//  particlesJS('landing-particles', {
-//    particles: {
-//      color: '#fff',
-//      shape: 'circle', // "circle", "edge" or "triangle"
-//      opacity:.7,
-//      size: 6,
-//      size_random: true,
-//      nb: 150,
-//      line_linked: {
-//        enable_auto: true,
-//        distance: 360,
-//        color: '#fff',
-//        opacity:.4,
-//        width: 2,
-//        condensed_mode: {
-//          enable: false,
-//          rotateX: 600,
-//          rotateY: 600
-//        }
-//      },
-//      anim: {
-//        enable: true,
-//        speed: 1.25
-//      }
-//    },
-//    interactivity: {
-//      enable: true,
-//      mouse: {
-//        distance: 300
-//      },
-//      detect_on: 'canvas', // "canvas" or "window"
-//      mode: 'grab',
-//      line_linked: {
-//        opacity: .4
-//      },
-//      events: {
-//        onclick: {
-//          enable: true,
-//          mode: 'push', // "push" or "remove"
-//          nb: 4
-//        }
-//      }
-//    },
-//    /* Retina Display Support */
-//    retina_detect: true
-//  });
-//}
+if(!bowser.android && !bowser.ios ) {
+  /* config dom id (optional) + config particles params */
+  particlesJS('landing-particles', {
+    particles: {
+      color: '#fff',
+      shape: 'circle', // "circle", "edge" or "triangle"
+      opacity:.7,
+      size: 6,
+      size_random: true,
+      nb: 150,
+      line_linked: {
+        enable_auto: true,
+        distance: 360,
+        color: '#fff',
+        opacity:.4,
+        width: 2,
+        condensed_mode: {
+          enable: false,
+          rotateX: 600,
+          rotateY: 600
+        }
+      },
+      anim: {
+        enable: true,
+        speed: 1.25
+      }
+    },
+    interactivity: {
+      enable: true,
+      mouse: {
+        distance: 300
+      },
+      detect_on: 'canvas', // "canvas" or "window"
+      mode: 'grab',
+      line_linked: {
+        opacity: .4
+      },
+      events: {
+        onclick: {
+          enable: true,
+          mode: 'push', // "push" or "remove"
+          nb: 4
+        }
+      }
+    },
+    /* Retina Display Support */
+    retina_detect: true
+  });
+}
 
 
 
