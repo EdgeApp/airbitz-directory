404eb52544c9614bc9c46745d678a01ce972aa0f
Damian <i3inary@gmail.com>
2015-08-19 14:09:12 -0700

    - Removed admin code from models.py and moved to admin.py - Fixed label on hidden bits promos

--- airbitz/notifications/admin.py
+++ airbitz/notifications/admin.py
@@ -18,7 +18,32 @@ class NotificationAdmin(admin.ModelAdmin):
     _get_message.allow_tags = True
 
 
+def format_string(obj, field):
+    style = "overflow:hidden; white-space:nowrap; max-width:300px; text-overflow:ellipsis; -o-text-overflow:ellipsis;"
+    return "<div style=\"{0}\">{1}</div>".format(style, (getattr(obj, field) or '').strip())
 
-admin.site.unregister(Notification)
+def format_message(obj):
+    return format_string(obj, 'message')
+
+def format_zero_message(obj):
+    return format_string(obj, 'zero_message')
+
+def format_tweet(obj):
+    return format_string(obj, 'tweet')
+
+format_message.allow_tags = True
+format_message.admin_order_field = 'message'
+format_zero_message.allow_tags = True
+format_zero_message.admin_order_field = 'zero_message'
+format_tweet.allow_tags = True
+format_tweet.admin_order_field = 'tweet'
+
+
+class HBitsPromosAdmin(admin.ModelAdmin):
+    list_display = ('token', format_message, format_zero_message, format_tweet, )
+    search_fields = ('token',)
+
+
 
 admin.site.register(Notification, NotificationAdmin)
+admin.site.register(HBitsPromos, HBitsPromosAdmin)
