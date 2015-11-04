67b64ca6b45a619746e64bab926ae3640c15617a
Tim Horton <tim@airbitz.co>
2015-11-04 10:25:27 -0800

    Added new fields to admin

--- airbitz/statistics/models.py
+++ airbitz/statistics/models.py
@@ -1,5 +1,6 @@
 from django.contrib.gis.db import models
 from django.contrib import admin
+import json
 
 class Event(models.Model):
     created = models.DateTimeField(auto_now_add=True)
@@ -8,6 +9,18 @@ class Event(models.Model):
     event_text = models.TextField(null=False)
     event_network = models.CharField(max_length=30)
 
+    def btc(self):
+        try:
+            return json.loads(self.event_text)['btc']
+        except:
+            return ''
+
+    def partner(self):
+        try:
+            return json.loads(self.event_text)['partner']
+        except:
+            return ''
+
     def __unicode__(self):
         return "{0}".format(self.event_text)
 
@@ -15,6 +28,8 @@ class EventAdmin(admin.ModelAdmin):
     list_display = ('created',
                     'event_type',
                     'event_network',
+                    'btc',
+                    'partner',
                     'event_text',
                     )
 
